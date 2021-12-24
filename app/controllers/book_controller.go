471e90ae1657d88826babaa815deeaeb005fc013
bocon <tan.truong@go2joy.vn>
2021-12-24 09:53:05 +0700

    Update - platform.database - Add database connection setup for PostgreSQL db

---
+++ app/controllers/book_controller.go
@@ -0,0 +1,14 @@
+package controllers
+
+// GetBooks func gets all exists books.
+// @Description Get all exists books.
+// @Summary Get all exists books
+// @Tags Books
+// @Accept json
+// @Product json
+// @Success 200 {array} models.Book
+// @Router /v1/books [get]
+//func GetBooks(c *fiber.Ctx) error {
+//	// Create database connection.
+//	db, err := database.Op
+//}
