b1536faf50ebd0e467ef049badc71ea3c055db28
bocon <tan.truong@go2joy.vn>
2021-12-24 14:22:33 +0700

    Update - pkg.configs - Add fiber_config

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+// @title API
+// @version 1.0
+// @description This is an auto-generated API docs.
+// @termsOfService http://swagger.io/terms/
+// @contact.name API Support
+// @contact.email your@mail.com
+// @license.name Apache 2.0
+// @license.url http://www.apache.org/licenses/LICENSE-2.0.html
+// @securityDefinitions.apikey ApiKeyAuth
+// @in header
+// @name Authorization
+// @BasePath /api
+func main() {
+	// Define Fiber config
+	config := configs.
+}
---
+++ pkg/configs/fiber_config.go
@@ -0,0 +1,20 @@
+package configs
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"os"
+	"strconv"
+	"time"
+)
+
+// FiberConfig func for configuration Fiber app.
+// See: https://docs.gofiber.io/api/fiber#config
+func FiberConfig() fiber.Config {
+	// Define server settings.
+	readTimeoutSecondsCount, _ := strconv.Atoi(os.Getenv("SERVER_READ_TIMEOUT"))
+
+	// Return Fiber configuration.
+	return fiber.Config{
+		ReadTimeout: time.Second * time.Duration(readTimeoutSecondsCount),
+	}
+}
