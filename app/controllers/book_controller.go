5b02e847ebb394103748d8b5b1fcbc22158c67ad
bocon <tan.truong@go2joy.vn>
2021-12-24 10:06:56 +0700

    Update - app.controllers.book_controller - Add GetBooks, GetBook controller

--- app/controllers/book_controller.go
+++ app/controllers/book_controller.go
@@ -1,5 +1,11 @@
 package controllers
 
+import (
+	"github.com/boconlonton/ho-golang-fiber-library-app/platform/database"
+	"github.com/gofiber/fiber"
+	"github.com/google/uuid"
+)
+
 // GetBooks func gets all exists books.
 // @Description Get all exists books.
 // @Summary Get all exists books
@@ -8,7 +14,82 @@ package controllers
 // @Product json
 // @Success 200 {array} models.Book
 // @Router /v1/books [get]
-//func GetBooks(c *fiber.Ctx) error {
-//	// Create database connection.
-//	db, err := database.Op
-//}
+func GetBooks(c *fiber.Ctx) error {
+	// Create database connection.
+	db, err := database.OpenDBConnection()
+	if err != nil {
+		// Return status 500 and database connection error.
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error": true,
+			"msg":   err.Error(),
+		})
+	}
+
+	// Get all books
+	books, err := db.GetBooks()
+	if err != nil {
+		// Return, if books not found.
+		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
+			"error": true,
+			"msg":   "books were not found",
+			"count": 0,
+			"books": nil,
+		})
+	}
+
+	// Return status 200 OK.
+	return c.JSON(fiber.Map{
+		"error": false,
+		"msg":   nil,
+		"count": len(books),
+		"books": books,
+	})
+}
+
+// GetBook func gets book by given ID or 404 error.
+// @Description Get book by given ID.
+// @Summary Get book by given ID.
+// @Tags Book
+// @Accept json
+// @Product json
+// @Param id path string true "Book ID"
+// @Success 200 {object} models.Book
+// @Router /v1/book/{id} [get]
+func GetBook(c *fiber.Ctx) error {
+	// Catch book ID from URL.
+	id, err := uuid.Parse(c.Params("id"))
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error": true,
+			"msg":   err.Error(),
+		})
+	}
+
+	// Create database connection.
+	db, err := database.OpenDBConnection()
+	if err != nil {
+		// Return status 500 and database connection error.
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error": true,
+			"msg":   err.Error(),
+		})
+	}
+
+	// Get book by ID.
+	book, err := db.GetBook(id)
+	if err != nil {
+		// Return, if nook not found
+		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
+			"error": true,
+			"msg":   "book with the given ID is not found",
+			"book":  nil,
+		})
+	}
+
+	// Return status 200 OK.
+	return c.JSON(fiber.Map{
+		"error": false,
+		"msg":   nil,
+		"book":  book,
+	})
+}
